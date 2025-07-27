199ecb15254460ebceebf395fcb64fa0ba813969
Abdigithub13 <abditolesa520@gmail.com>
2025-07-27 22:21:06 +0300

    updated

--- ERP_HR/frontend/src/app/hr-module/approve-dept-to/page.tsx
+++ ERP_HR/frontend/src/app/hr-module/approve-dept-to/page.tsx
@@ -869,7 +869,6 @@ function ApproveDeptTo() {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
             <div className="space-y-4">
               <div className="h-4" />
-              {/* Progress by  */}
               <div className="flex flex-col sm:flex-row items-stretch sm:items-center gap-2 justify-start mt-2">
                 <label className="block text-xs font-medium text-gray-700 mb-0 whitespace-nowrap min-w-[120px]">
                   Processed by:
